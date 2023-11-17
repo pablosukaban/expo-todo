95dda8f15a3222e2cbdcf3f53190863e48dc97c4
ilya <zapaska96@gmail.com>
2023-11-17 21:43:11 +0500

    добавил отрисовку списка дел

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { todoTasks } from './constants';
+import CheckBox from 'expo-checkbox';
+import { useState } from 'react';
+
+const TodoItem = ({ task }) => {
+  const [value, setValue] = useState(task.completed ?? false);
+
+  return (
+    <View style={styles.todoItem}>
+      <CheckBox value={value} onChange={() => setValue((prev) => !prev)} />
+      <Text>{task.title}</Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Todo App</Text>
+      <View style={styles.todoList}>
+        {todoTasks.map((task) => (
+          <TodoItem key={task.id} task={task} />
+        ))}
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,6 +34,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'space-between',
+    padding: 20,
+    margin: 20,
+    gap: 20,
+    border: '1px solid black',
+  },
+  todoList: {
+    // width: '100%',
+    height: '100%',
+  },
+  todoItem: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 30,
   },
 });
---
+++ constants.js
@@ -0,0 +1,22 @@
+export const todoTasks = [
+  {
+    id: 1,
+    title: 'Learn React Native',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Learn React',
+    completed: true,
+  },
+  {
+    id: 3,
+    title: 'Learn JavaScript',
+    completed: false,
+  },
+  {
+    id: 4,
+    title: 'Learn GraphQL',
+    completed: false,
+  },
+];
