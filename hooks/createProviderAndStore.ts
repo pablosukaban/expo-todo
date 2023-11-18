926d5375af6bdc6f61dee212fca6cf24f0438ed2
ilya <zapaska96@gmail.com>
2023-11-18 12:02:32 +0500

    исправил типизацию

--- hooks/createProviderAndStore.ts
+++ hooks/createProviderAndStore.ts
@@ -1,13 +1,16 @@
 import { createContext, useContext } from 'react';
 
-export const createProviderAndStore = <T extends new () => any>(
+export const createProviderAndStore = <
+  T extends new () => any,
+  S extends InstanceType<T>
+>(
   storeClass: T
 ) => {
   const store = new storeClass();
 
   const StoreContext = createContext(store);
 
-  const useStore = () => {
+  const useStore = (): S => {
     const store = useContext(StoreContext);
     if (!store) {
       throw new Error('Store not found');
